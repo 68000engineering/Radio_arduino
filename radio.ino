d3bd6feff4b003de35d5b332db8f182ccc67737b
68000engineering <68000.engineering.group@gmail.com>
2025-11-26 02:28:14 +0100

    mk. 1
    pre-release xD

--- radio.ino
+++ radio.ino
@@ -1,30 +1,39 @@
 #include <Wire.h>       //I2C Lib for si4703
-#include <radio.h>      //lib for si4703
-#include <si4703.h>     //driver for radio
-#include <TM1637Display.h>
+#include <TM1637Display.h>//Lib display
+#include <Si4703_Breakout.h> //Lib for radio
 
-#define sup 3       //sation up on pin3
-#define sdn 4       //station down on pin4
-#define vup 5       //volume down on pin5
-#define vdn 6       //volume down on pin5
+#define sup 4       //sation up on pin3
+#define sdn 5       //station down on pin4
+#define vup 6       //volume down on pin5
+#define vdn 7       //volume down on pin5
 #define CLK 11      //displ CLK on pin 11
 #define DIO 12      //displ DIO on pin 12
 
+int resetPin = 2;
+int SDIO = A4;
+int SCLK = A5;
+
+Si4703_Breakout radio(resetPin, SDIO, SCLK);
+
 using namespace std;
 
-SI4703 radio;
+
 TM1637Display display = TM1637Display(CLK, DIO);
 
-int station = 10350;
+int station = 1035;
 int volume = 4;
 
 void increaseVolume(){
-  if(volume<100) volume++;
+  volume ++;
+  if (volume == 16) volume = 15;
+    radio.setVolume(volume);
   display.showNumberDec(volume);
 }
 
 void decreaseVolume(){
-  if(volume>0) volume--;
+  volume --;
+  if (volume < 0) volume = 0;
+    radio.setVolume(volume);
   display.showNumberDec(volume);
 }
 
@@ -35,17 +44,13 @@ pinMode(vdn, INPUT_PULLUP);
 pinMode(sup, INPUT_PULLUP);
 pinMode(sdn, INPUT_PULLUP);
 
-Serial.begin(115200);
+Serial.begin(9600);
 display.clear();
 
-/*
-radio.init();
-radio.setBandFrequency(RADIO_BAND_FM, station);
-radio.setVolume(volume);
-radio.setMono(false);
-radio.setMute(false);
-*/
+radio.powerOn();
+radio.setVolume(4);
 }
+
 void loop() {
 Serial.print("Station - ");
 Serial.println(station, DEC);
@@ -55,27 +60,23 @@ Serial.println(volume, DEC);
 display.setBrightness(7);
 
 if(digitalRead(sup) == 0){
-  station += 100;
-  int station_disp = station/10;
-  display.showNumberDec(station_disp);
-  //radio.setBandFrequency(RADIO_BAND_FM, station);
+  station += 1;
+  radio.setChannel(station);
+  display.showNumberDec(station);
 }
 
 if(digitalRead(sdn) == 0){
-  station -= 100;
-  int station_disp = station/10;
-  display.showNumberDec(station_disp);
-  //radio.setBandFrequency(RADIO_BAND_FM, station);
+  station -= 1;
+  radio.setChannel(station);
+  display.showNumberDec(station);
 }
 
 if(digitalRead(vup) == 0){
   increaseVolume();
-  //radio.setVolume(volume);
 }
 
 if(digitalRead(vdn) == 0){
   decreaseVolume();
-  //radio.setVolume(volume);
 }
 
 delay(200);
